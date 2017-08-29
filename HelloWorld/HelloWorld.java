4e391587609e92538953663acbec2522675f8469
Zannadander <31426469+Zannadander@users.noreply.github.com>
2017-08-28 23:23:36 -0400

    Newlines!

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		System.out.print("Hello World\n");
 		System.out.println("Some more");
 	}
 
